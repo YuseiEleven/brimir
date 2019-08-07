9421e1888ab7ef69ad0ae4fa4a722aabfeaa0056
YuseiEleven <yusei.eleven@gmail.com>
2019-08-07 18:10:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,31 +1,42 @@
-const { Client, Util } = require('discord.js');
-const PREFIX = '!';
-const ytdl = require('ytdl-core');
- 
-const client = new Client({ disableEveryone: true });
- 
-client.on('ready', () => console.log('Ready!'));
- 
-client.on('message', async msg => {
-        if (msg.author.bot) return;
-        if (!msg.content.startsWith('!')) return
-        const args = msg.content.split(' ')
-        let command = msg.content.toLowerCase().split(' ')[0]
-        command = command.slice(PREFIX.length)
- 
-        if (command === 'play') {
-                const voiceChannel = msg.member.voiceChannel;
-                if (!voiceChannel) return msg.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
-                voiceChannel.join()
-                        .then(connection => {
-                                const dispatcher = connection.playStream(ytdl(args[1]))
-                                console.log(dispatcher.time)
-                        })
-                        .catch(console.log)
-        }
-});
- 
+const Discord = require('discord.js');
+const client = new Discord.Client();
 client.login(process.env.token);
+
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1 };
+
+client.on('ready', () => {
+	console.log("Music Bot is ready!");
+});
+
+
+client.on('message', message => {
+
+	if(message.author.bot)
+		return;
+	
+	if(message.content.toLowerCase().stratsWith("!play"))
+	{
+		let args = message.content.split(" ");
+		let url = args[1];
+		let VoiceChannel = message.guild.channels.find(channel => channel.id === '603549810397544474');
+		if(VoiceChannel != null)
+		{
+			console.log(VoiceChannel.name + " was found and is a " + VoiceChannel.type + " channel.");
+			VoiceChannel.join()
+			.then(connection => {
+				console.log("Bot joined the channel.");
+				const stream = ytdl(url, { filter : 'audioonly' });
+				const dispatcher = connection.playStream(stream, streamOptions);
+				
+				dispatcher.on('end', () => {
+					VoiceChannel.leave();
+				})
+			})
+			.catch();
+		}
+	}
+});
 
 
 
