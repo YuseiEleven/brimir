e6d52b3654922d8af2f9e2c5d4b67ab81119a4fc
YuseiEleven <yusei.eleven@gmail.com>
2019-08-07 14:57:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -169,3 +169,30 @@ bot.on("message", function (msg) {
         }
     }
 });
+
+const { Client, Util } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
+const ytdl = require('ytdl-core');
+ 
+const client = new Client({ disableEveryone: true });
+ 
+client.on('ready', () => console.log('Ready!'));
+
+client.on('message', async msg => {
+        if (msg.author.bot) return;
+        if (!msg.content.startsWith('!')) return
+        const args = msg.content.split(' ')
+        let command = msg.content.toLowerCase().split(' ')[0]
+        command = command.slice(PREFIX.length)
+ 
+        if (command === 'play') {
+                const voiceChannel = msg.member.voiceChannel;
+                if (!voiceChannel) return msg.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+                voiceChannel.join()
+                        .then(connection => {
+                                const dispatcher = connection.playStream(ytdl(args[1]))
+                                console.log(dispatcher.time)
+                        })
+                        .catch(console.log)
+        }
+});
