eb2bfc6393c561d9064ac39b642d25235ff7f46e
YuseiEleven <yusei.eleven@gmail.com>
2019-08-07 14:39:36 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -175,7 +175,7 @@ bot.on("message", function (msg) {
 
 
 const { Client, Util } = require('discord.js');
-const PREFIX = ! ;
+const PREFIX = !;
 const GOOGLE_API_KEY = (process.env.APIKEY);
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
